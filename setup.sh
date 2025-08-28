36e6ca9f8ad9cb0f4ba5a22c79aa29d6cf017238
ALICE HARTING <aharting@kth.se>
2025-08-28 08:55:14 +0200

    Cleanup

--- setup.sh
+++
@@ -1,4 +0,0 @@
-source activate base
-conda create -n "venv" python=3.8.10
-conda activate venv
-pip install -r requirements.txt
--- train_and_eval.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#python orchestration/train_openloop_model.py --config configs/openloop.yaml
-#python orchestration/evaluation_openloop.py --config configs/openloop.yaml --max_unravel 10
-
-#python orchestration/train_closedloop_model.py --config configs/closedloop.yaml
-#python orchestration/evaluation_closedloop.py --config configs/closedloop.yaml
-
-python orchestration/evaluation_closedloop_report.py --config configs/closedloop_report.yaml
